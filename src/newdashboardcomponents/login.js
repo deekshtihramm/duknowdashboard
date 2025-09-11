78106f94b12c151875f1707f26354f315e22e88e
golozap <deekshith6675@gmail.com>
2025-09-11 22:56:42 +0530

    newdashboard5

--- src/newdashboardcomponents/login.js
+++ src/newdashboardcomponents/login.js
@@ -21,7 +21,7 @@ export default function ProfessionalLoginPage({ onLogin }) {
 
     try {
       setLoading(true);
-      const res = await fetch('http://localhost:4000/api/dashboard/signin', {
+      const res = await fetch('https://web.backend.duknow.in/api/dashboard/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: email.trim(), password, remember }),
